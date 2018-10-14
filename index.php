6c7beca3eadcb766d5ee805850e7d06205b178e1
Carruthers <C7577029@systems.private>
2018-10-14 23:29:03 +0100

    Added SR

--- index.php
+++ index.php
@@ -27,7 +27,8 @@ $img = imagecreatetruecolor(200, 200);
 echo "<h1>The Support Monster On-Call Rota</h1><h3>" . date("d/m/Y") . "</h3><br/>";
 //echo "The time is " . date("d/m/Y") . "<br>";
 
-$faces = array("Kevin", "Gary", "Eaps", "Vic", "Peter", "Alix", "Amit", "Zed");
+//$faces = array("Kevin", "Gary", "Eaps", "Vic", "Peter", "Alix", "Amit", "Zed");
+$faces = array("Kevin", "Gary", "Eaps", "Vic", "Peter", "Alix", "Amit", "Zed", "Sridhar");
 foreach ($faces as $face) {
 	echo "    <a href='./images/$face.png'>$face</a><br/>";
 }
