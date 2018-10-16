39ffeca0c81b82f2e9d997451bbb9e099c9a6e29
c7577029 <43537408+c7577029@users.noreply.github.com>
2018-10-17 00:57:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 echo "<h1>The Support Monster On-Call Rota</h1><h3>" . date("d/m/Y") . "</h3><br/>";
 //echo "The time is " . date("d/m/Y") . "<br>";
 
-$faces = array("Kevin", "Gary", "Eaps", "Vic", "Peter", "Alix", "Amit", "Zed");
-//$faces = array("Kevin", "Gary", "Eaps", "Vic", "Peter", "Alix", "Amit", "Zed", "Sridhar");
+//$faces = array("Kevin", "Gary", "Eaps", "Vic", "Peter", "Alix", "Amit", "Zed");
+$faces = array("Kevin", "Gary", "Eaps", "Vic", "Peter", "Alix", "Amit", "Zed", "Sridhar");
 foreach ($faces as $face) {
 	echo "    <a href='./images/$face.png'>$face</a><br/>";
 }
