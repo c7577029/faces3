f2e018531c9b497698eca07eea8cf5aae3ebce6f
c7577029 <43537408+c7577029@users.noreply.github.com>
2018-10-17 15:01:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 echo "<h1><u>The UK & NL Monster Support On-Call Rota</u></h1><h3>" . date("d/m/Y") . "</h3><br/>";
 //echo "The time is " . date("d/m/Y") . "<br>";
 
-$faces = array("1) Kevin", "2) Gary", "3) Eaps", "4) Vic", "5) Peter", "6) Alix", "7) Amit", "8) Zed");
+$faces = array("Kevin", "Gary", "Eaps", "Vic", "Peter", "Alix", "Amit", "Zed");
 //$faces = array("Kevin", "Gary", "Eaps", "Vic", "Peter", "Alix", "Amit", "Zed", "Sridhar");
 
 echo "<h2>Select support resource from list</h2><br/>";
